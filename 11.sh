586a75bf8cc7ac20ff33f262c95ae5d7ca8a1adf
CahyaGans <56258686+CahyaGanteng123@users.noreply.github.com>
2019-10-08 18:44:13 +0700

    Add files via upload

---
+++ 11.sh
@@ -0,0 +1,103 @@
+#usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+echo $pur " _____    "
+echo $pur "/  _/\\  "
+echo $pur "| / oo"
+echo $pur "\(   _\ "
+echo $pur " \  O/"
+echo $pur " /   \ "
+echo $pur " ||  ||"
+echo $pur " ||  ||      'Mmpsh Ahhhhh..' "
+echo $pur " ||  || _____ /"
+echo $pur " | \ ||(___  )    'Enak Mass..' "
+echo $pur "// / \|_)o (  )"
+echo $pur "\\ ///|)\_(    )  'Ahhh... Uhh..' "
+echo $pur "||   |\ )(    )"
+echo $pur "||   ) \/(____)_      ___"
+echo $pur " ||   |\___/     \---/ \.\."
+echo $pur " ||   | :   _       ./   ))"
+echo $pur " ||   | \../ \~~~-./   ./__ _"
+echo $pur " \\  /           \.______  ( ("
+echo $pur " ((___ooO                \._\_\ "
+
+echo $cy"BY : MR.$€V£N"
+echo $cy"Masukkan Nama Lu"
+read p
+echo $me"Masukkan Nama Korban Yang Mau DiEntot"
+read r
+echo $i"Masukkan Alamat Untuk DiEntot Nanti"
+read u
+echo $pur"Mau Masukkin Dari Atas Apa Bawah"
+read t
+echo $cy"Masukkan Jenis Gaya Contoh*Mengangkang*"
+read cb
+echo $i"Mau Punya Anak Berapa?"
+read nak
+echo $cy"Loading..."
+sleep 4
+clear
+echo $cy"$r : Assalamualaikum Sayank"
+sleep 1
+echo $me"$p : Walaikumsalam,$r Ada Apa Yank?"
+sleep 1
+echo $cy"$r : Yank...,Dirumah Aku Kan Sepi Emak Aku Kondangan"
+sleep 1
+echo $me"$p : Ouh Aku Mah Udh Tau Itumah Yank Yuk,Tunggu Ya Aku Otw"
+sleep 1
+echo $cy"$r : Iya Sayank Aku Siap² Buka Baju Dulu Yaaa..."
+sleep 2
+echo $i"Assalamualaikum $r"
+sleep 1
+echo $i"Walaikumsalam,Ayuk Yank Aku Udh Buka Baju Nihhh.. "
+sleep 1
+echo $i"$r Ayok Yank Aku Udh Gaceng Fulll nihh..."
+sleep 2
+echo $me"Menuju KeKamar..."
+sleep 4
+echo $pu"$r Coliin Aku Dulu Dong :v"
+sleep 1
+echo $ku"$p Enak Ya Sayank Tunggu Sampe Ngecrot yaa"
+sleep 1
+echo $i"Aku Pegang Toket Mu Ya Toket Mu Besar :v"
+echo $i"Ayo Yank Aku Udh Ngecrot Ini"
+echo $ku"Iya Sayank Sabar Dong :("
+sleep 2
+echo "Ahh Ahh Ahhh" | lolcat
+echo "Mpshh Ahhh" | lolcat
+sleep 2
+echo "ahhh Ahhh Ahhhh" | lolcat
+echo "Mpsh Ahhhh Ahh😥" | lolcat
+sleep 5
+echo "Ikehh Ahh Ahh. Ayo Kuatin Lagi Bang, Ahh" | lolcat
+echo "ikeh Ikeh. Mpsssss Ahhhh" | lolcat
+sleep 5
+echo "Ahhhhh Ahhhhhh😣" | lolcat
+echo "Masukin Ya Bener Dong😒" | lolcat
+sleep 7
+echo "*sfx : Plok Plok Plok Plok Plok Plok" | lolcat
+sleep 3
+echo " Pelan Pelan Sayank ☹️" | lolcat
+sleep 7
+echo "Ahhh Kok Keluar Di Dalem😭" | lolcat
+echo "Udahh Bang Aku Udh Lemes :(" | lolcat
+echo "Aku Udh Gak Kuat Sayang :(" | lolcat
+echo ""
+echo ""
+echo ""
+echo $i"NGEWE ONLINE SUKSES!"
+echo ""
+echo ""
+echo $ku"Berhasil $p Punya Anak $nak,$p Si $r Minta DiEntot"
+echo $ku"Lagi Katanya"
+echo ""
+echo $me"DOSA TANGGUNG SENDIRI AJG!"
+exit
