873ac7d17a8195a3397eebcbe4db9119708aed73
CahyaGans <56258686+CahyaGanteng123@users.noreply.github.com>
2019-10-08 18:05:44 +0700

    Add files via upload

---
+++ GABUTCUK.sh
@@ -0,0 +1,68 @@
+#usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+toilet -f big -F gay Tools GABUT
+echo "<=========================>" | lolcat
+echo "Author  : MR.$€V£N" | lolcat
+echo "Team    : Ninja Cyber Army" | lolcat
+echo "Youtube : CahyaXLocos" | lolcat
+echo "Whatsap : 62895389743582" | lolcat
+echo "<=========================>" | lolcat
+echo ""
+sleep 1
+echo $ku"TOOLS GABUT NIH JENG :V"
+sleep 1
+echo $pur"<#DAFTAR TOOLS#>"
+sleep 1
+echo $cy"================="
+echo $i"1.Ngewe Online"
+sleep 1
+echo $cy"================="
+echo $ku"2.Santet Online"
+sleep 1
+echo $cy"================="
+echo $pu"3.Bermain Game"
+sleep 1
+echo $cy"================="
+echo $pur"4.Kembali"
+sleep 1
+echo $cy"================="
+echo $me"5.Keluar"
+sleep 1
+echo $cy"================="
+echo $i"Pilih Nomor"
+read -p"====> " pil;
+
+if [ $pil = 1 ]
+then
+sh 11.sh
+fi
+
+if [ $pil = 2 ]
+then
+sh 12.sh
+fi
+
+if [ $pil = 3 ]
+then
+sh 14.sh
+fi
+
+if [ $pil = 4 ]
+then
+sh CahyaTOOLS.sh
+fi
+
+if [ $pil = 5 ]
+then
+echo $i"Terima"$ku "Kasih Bro"
+exit
+fi
